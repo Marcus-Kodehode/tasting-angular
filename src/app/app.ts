10079e3d5c7c3be3f74ff22fc0b17ccecd3ff164
Marcus <marcus29sfj@gmail.com>
2025-08-25 15:00:55 +0200

    Mocked reality convincingly

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common'; // 🔥 LEGG TIL DENNE
 import { Todo } from './todo/todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Todo],
+  imports: [Todo, FormsModule, NgIf], // 👈 Legg til NgIf her også
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  name = 'Marcus';
+  name = '';
+  tempName = '';
+
+  ngOnInit() {
+    const stored = localStorage.getItem('user:name');
+    if (stored) {
+      this.name = stored;
+    }
+  }
+
+  saveName() {
+    const trimmed = this.tempName.trim();
+    if (!trimmed) return;
+    this.name = trimmed;
+    localStorage.setItem('user:name', this.name);
+  }
 }
